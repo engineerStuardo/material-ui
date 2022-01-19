46a499b0d14a8610d170053226d980719eaead6e
engineerStuardo <ingeniero.stuardo@gmail.com>
2022-01-18 22:42:50 -0600

    creating Header component and start working with AppBar from MUI

---
+++ src/components/ui/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography, useScrollTrigger } from '@mui/material';
+
+function ElevationScroll(props) {
+  const { children } = props;
+
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
+const Header = () => {
+  return (
+    <ElevationScroll>
+      <AppBar position='fixed' color='primary'>
+        <Toolbar>
+          <Typography variant='h6'>Arc Development</Typography>
+        </Toolbar>
+      </AppBar>
+    </ElevationScroll>
+  );
+};
+
+export default Header;
